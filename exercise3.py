2bf43ad3608e3d5170844e4d211d158973a700ee
jiya <jiya.kanhirathanpoyil@metropolia.fi>
2024-09-15 15:39:50 +0300

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -49,7 +49,7 @@ else:
 ####################4
 
 year = int(input("please type in your YEAR: "))
-if year % 400 == 0 or year % 4 ==0 and year % 100 != 0:
+if (year % 400 == 0) or (year % 4 ==0 and year % 100 != 0):
         print(f"year : {year} is a leap year")
 else:
     print(f"year: {year} not a leap year")
